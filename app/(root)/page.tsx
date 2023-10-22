7e2256971d6fdf5b7d5e7be77c268957b8a9495d
Ron Almog <ron8132@gmail.com>
2023-10-22 19:21:31 -0400

    add basic tailwind

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import { JapaneseYen } from "lucide-react";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="p-6">
+      <p>asdf</p>
+      <Button variant="destructive">click me</Button>
+    </div>
+  );
+}
