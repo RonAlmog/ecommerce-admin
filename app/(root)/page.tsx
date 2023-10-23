d063753bfc8947a22530a9144561898d6de19aab
Ron Almog <ron8132@gmail.com>
2023-10-22 22:52:56 -0400

    add auth pages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,13 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 import { JapaneseYen } from "lucide-react";
 import Image from "next/image";
 
-export default function Home() {
+export default function SetupPage() {
   return (
-    <div className="p-6">
-      <p>asdf</p>
-      <Button variant="destructive">click me</Button>
+    <div className="p-4">
+      <p>protected page</p>
+      <UserButton afterSignOutUrl="/" />
     </div>
   );
 }
