f6e137b058a19fd5d3fb3b845193555fabf96b54
Ron Almog <ron8132@gmail.com>
2023-10-25 10:25:49 -0400

    store switcher popover trigger

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,6 @@
 import { UserButton } from "@clerk/nextjs";
 import MainNav from "@/components/main-nav";
+import StoreSwitcher from "@/components/store-switcher";
 
 type Props = {};
 
@@ -7,7 +8,7 @@ const Navbar = (props: Props) => {
   return (
     <div className="border-b">
       <div className="flex h-16 items-center px-4">
-        <div>store switcher</div>
+        <StoreSwitcher />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
---
+++ components/store-switcher.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { Store } from "@prisma/client";
+import { Popover, PopoverTrigger } from "./ui/popover";
+import { useStoreModal } from "@/hooks/use-store-modal";
+import { useParams, useRouter } from "next/navigation";
+import { useState } from "react";
+import { Button } from "./ui/button";
+import { ChevronsUpDown, Store as StoreIcon } from "lucide-react";
+import { cn } from "@/lib/utils";
+
+type PopoverTriggerProps = React.ComponentPropsWithoutRef<
+  typeof PopoverTrigger
+>;
+
+interface StoreSwitcherProps extends PopoverTriggerProps {
+  items: Store[];
+}
+
+const StoreSwitcher = ({ className, items = [] }: StoreSwitcherProps) => {
+  const storeModal = useStoreModal();
+  const params = useParams();
+  const router = useRouter();
+
+  const formattedItems = items.map((item) => ({
+    label: item.name,
+    value: item.id,
+  }));
+
+  const currentStore = formattedItems.find(
+    (item) => item.value === params.storeId
+  );
+
+  const [open, setOpen] = useState(false);
+
+  const onStoreSelect = (store: { value: string; label: string }) => {
+    setOpen(false);
+    router.push(`/${store.value}`);
+  };
+
+  return (
+    <Popover open={open} onOpenChange={setOpen}>
+      <PopoverTrigger asChild>
+        <Button
+          variant="outline"
+          size="sm"
+          role="combobox"
+          aria-expanded={open}
+          className={cn("w-[200px] justify-between", className)}
+        >
+          <StoreIcon className="mr-2 h-4 w-4" />
+          Current Store
+          <ChevronsUpDown className="h-4 w-4 ml-auto shrink-0 opacity-50" />
+        </Button>
+      </PopoverTrigger>
+    </Popover>
+  );
+};
+
+export default StoreSwitcher;
