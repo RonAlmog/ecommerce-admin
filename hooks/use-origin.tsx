0c9876b5acf134be6c5cb1613391f565ab4c8ac4
Ron Almog <ron8132@gmail.com>
2023-10-26 19:53:18 -0400

    add use origin

---
+++ hooks/use-origin.tsx
@@ -0,0 +1,20 @@
+// access the window object on server side.
+import { useState, useEffect } from "react";
+export const useOrigin = () => {
+  const [mounted, setMounted] = useState(false);
+
+  const origin =
+    typeof window !== "undefined" && window.location.origin
+      ? window.location.origin
+      : "";
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return "";
+  }
+
+  return origin;
+};
