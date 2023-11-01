007665d0abea96ba56651ef883ecb66f1e2ec516
Ron Almog <ron8132@gmail.com>
2023-11-01 16:46:58 -0400

    fix cell actions for color

--- app/(dashboard)/[storeId]/(routes)/colors/_components/cell-action.tsx
+++ app/(dashboard)/[storeId]/(routes)/colors/_components/cell-action.tsx
@@ -7,7 +7,7 @@ import {
   DropdownMenuLabel,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { SizeColumn } from "./columns";
+import { ColorColumn } from "./columns";
 import { Button } from "@/components/ui/button";
 import { Copy, Edit, MoreHorizontal, Trash } from "lucide-react";
 import toast from "react-hot-toast";
@@ -17,7 +17,7 @@ import axios from "axios";
 import AlertModal from "@/components/modals/alert-modal";
 
 interface CellActionProps {
-  data: SizeColumn;
+  data: ColorColumn;
 }
 
 const CellAction = ({ data }: CellActionProps) => {
@@ -33,12 +33,12 @@ const CellAction = ({ data }: CellActionProps) => {
   const onDelete = async () => {
     try {
       setLoading(true);
-      await axios.delete(`/api/${params.storeId}/sizes/${data.id}`);
+      await axios.delete(`/api/${params.storeId}/colors/${data.id}`);
       router.refresh();
-      toast.success("Size deleted");
+      toast.success("color deleted");
     } catch (error) {
       toast.error(
-        "Make sure you remove all your products using this size first"
+        "Make sure you remove all your products using this color first"
       );
     } finally {
       setLoading(false);
@@ -68,7 +68,7 @@ const CellAction = ({ data }: CellActionProps) => {
             Copy Id
           </DropdownMenuItem>
           <DropdownMenuItem
-            onClick={() => router.push(`/${params.storeId}/sizes/${data.id}`)}
+            onClick={() => router.push(`/${params.storeId}/colors/${data.id}`)}
           >
             <Edit className="mr-2 h-4 w-4" />
             Update
