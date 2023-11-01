c2db93aa8829d89339e14f740e1df16789c48f29
Ron Almog <ron8132@gmail.com>
2023-11-01 16:35:03 -0400

    add api endpoints for color

---
+++ app/api/[storeId]/colors/[colorId]/route.ts
@@ -0,0 +1,118 @@
+import prismadb from "@/lib/prismadb";
+import { auth } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+// get the color by id.
+// no auth, open to all.
+export async function GET(
+  req: Request,
+  { params }: { params: { colorId: string } }
+) {
+  try {
+    if (!params.colorId) {
+      return new NextResponse("colorId is required", { status: 400 });
+    }
+
+    const color = await prismadb.color.findUnique({
+      where: {
+        id: params.colorId,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLOR_GET]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+export async function PATCH(
+  req: Request,
+  { params }: { params: { storeId: string; colorId: string } }
+) {
+  try {
+    const { userId } = auth();
+    const body = await req.json();
+    const { name, value } = body;
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+    if (!name || !value) {
+      return new NextResponse("name and value are required", {
+        status: 400,
+      });
+    }
+
+    if (!params.colorId) {
+      return new NextResponse("colorId is required", { status: 400 });
+    }
+
+    // make sure store belongs to user
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const color = await prismadb.color.updateMany({
+      where: {
+        id: params.colorId,
+      },
+      data: {
+        name,
+        value,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLOR_PATCH]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
+
+// params must be the second param.
+// so req is there even tho not used.
+export async function DELETE(
+  req: Request,
+  { params }: { params: { storeId: string; colorId: string } }
+) {
+  try {
+    const { userId } = auth();
+    if (!userId) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    if (!params.colorId) {
+      return new NextResponse("colorId is required", { status: 400 });
+    }
+
+    // make sure store belongs to user
+    const storeByUserId = await prismadb.store.findFirst({
+      where: {
+        id: params.storeId,
+        userId,
+      },
+    });
+
+    if (!storeByUserId) {
+      return new NextResponse("Unauthorized", { status: 403 });
+    }
+
+    const color = await prismadb.color.deleteMany({
+      where: {
+        id: params.colorId,
+      },
+    });
+
+    return NextResponse.json(color);
+  } catch (error) {
+    console.log("[COLOR_DELETE]", error);
+    return new NextResponse("Internal Error", { status: 500 });
+  }
+}
