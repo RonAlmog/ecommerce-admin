8c8eb8adea27bf97db10f9a2bef9d00d9308a728
Ron Almog <ron8132@gmail.com>
2023-11-02 20:52:48 -0400

    final fixes for products page

--- app/(dashboard)/[storeId]/(routes)/products/_components/product-client.tsx
+++ app/(dashboard)/[storeId]/(routes)/products/_components/product-client.tsx
@@ -30,7 +30,7 @@ const ProductClient = ({ data }: ProductClientProps) => {
         </Button>
       </div>
       <Separator />
-      <DataTable columns={columns} data={data} searchKey="label" />
+      <DataTable columns={columns} data={data} searchKey="name" />
       <Heading title="API" description="API calls for products" />
       <ApiList entityName="products" entityIdName="productId" />
     </>
